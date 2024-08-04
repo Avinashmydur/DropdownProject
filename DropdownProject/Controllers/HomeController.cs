d89076d95e9c161893dc0a032a564547bdc052a4
Avinash <Avinash@LAPTOP-HAIAHBJF>
2024-08-04 13:33:46 +0530

    Add project files.

---
+++ DropdownProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DropdownProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropdownProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Student stud, string csharp, string java, string php)
+        {
+            string course=string.Empty;
+            if (csharp == "true")
+            {
+                course = course + "C#" + ",";
+            }
+            if (java == "true")
+            {
+                course = course + "Java" + ",";
+            }
+            if (php == "true")
+            {
+                course = course + "PHP" + ",";
+            }
+            stud.Country = course;
+            Student obj=new Student();
+            obj.AddStudent(stud);
+            return View();
+        }
+    }
+}
---
+++ DropdownProject/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DropdownProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public SqlConnection connection()
+        {
+            //add connectionstring in web.config file
+            /*  </appSettings>
+	                <ConnectionStrings>
+		                <add name="ConStringName" ConnectionString="data source=LAPTOP-HAIAHBJF\SQLEXPRESS;database=Student;Integrated Security=True" providerName="System.Data.SqlClient"/>
+	                </ConnectionStrings>
+                <system.web> 
+             */
+            string _connectionString =ConfigurationManager.ConnectionStrings["ConStringName"].ConnectionString;
+            SqlConnection con=new SqlConnection(_connectionString);
+            return con;
+        }
+        //Add Student data
+        public void AddStudent(Student stu)
+        {
+            //create connection
+            var con = connection();
+            //open the connection
+            con.Open();
+            //pass the query
+            string query = "insert into studentDetails values('" + stu.Id + "','" + stu.Name + "','" + stu.City + "','" + stu.Gender + "','" + stu.Country + "')";
+            SqlCommand cmd= new SqlCommand(query,con);
+            //execute the query
+            cmd.ExecuteNonQuery();
+            //close the connection
+            con.Close();
+        }
+    }
+}
